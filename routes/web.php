c9af6d141a576a62f09511b5bf83836dcffcaa55
Danx398 <danidannniel398@gmail.com>
2022-11-08 14:02:49 -0600

    v1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Registros;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/registrar',[Registros::class,'index'])->name('registrar');
