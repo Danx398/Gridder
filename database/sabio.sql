c51b495809baf571173418b8fe801caf31fb452e
Danx398 <danidannniel398@gmail.com>
2022-11-11 22:30:30 -0600

    v1

--- database/sabio.sql
+++ database/sabio.sql
@@ -19,6 +19,7 @@
 -- Table structure for table `failed_jobs`
 --
 CREATE DATABASE laPalabraDelSabio;
+use laPalabraDelSabio;
 DROP TABLE IF EXISTS `failed_jobs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -135,7 +136,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
