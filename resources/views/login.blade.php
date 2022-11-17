f16e9b1569c369d6f0a40b7a39533b19629db11d
Danx398 <danidannniel398@gmail.com>
2022-11-16 19:20:46 -0600

    v1

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <div class="container mt-5">
         <div class="row mt-5">
             <div class="col"></div>
-            <div class="col-4 shadow rounded mt-5 border">
+            <div class="col-4 shadow rounded mt-5 bg-secondary bg-gradient text-light">
                 <h1 class="text-center mb-3 mt-3">INICIAR SESION</h1>
                 <form action="{{route('logear')}}" method="post" autocomplete="off">
                     @csrf
@@ -16,13 +16,13 @@
                     <label for="pass">Contraseña</label>
                     <div class="input-group mt-1 mb-3">
                         <span class="input-group-text" for="pass"><i class="fa-solid fa-key"></i></span>
-                        <input required="" type="password" class="form-control" name="password" id="pasword" value="{{ old("password") }}">
+                        <input required="" type="password" class="form-control" name="password" id="pasword" placeholder="*****" value="{{ old("password") }}">
                     </div>
                     <div class="d-flex justify-content-center">
-                        <button class=" btn btn-outline-dark mb-3">Iniciar sesion</button>
+                        <button class=" btn btn-outline-light mb-3">Iniciar sesion</button>
                     </div>
                 </form>
-                <a class="d-flex justify-content-center mb-3" href="{{route('registro')}}">Registrarse</a>
+                <a class="d-flex justify-content-center mb-3 text-light" href="{{route('registro')}}">Registrarse</a>
             </div>
             <div class="col"></div>
         </div>
