6136dbdccc5048b1249aa5aa8730dd46c58f6ca8
Danx398 <danidannniel398@gmail.com>
2022-11-22 13:05:47 -0600

    new version

--- resources/views/lib/cienAnios.blade.php
+++ resources/views/lib/cienAnios.blade.php
@@ -3,7 +3,7 @@
 @section('contenido')
 <div class="container">
     <div class="row">
-        <div class="col">
+        <div class="col text-light">
             <h1 class="text-center">Cien años de Soledad</h1>
             <h3>Sinopsis</h3>
             <p>Entre la boda de José Arcadio Buendía con Amelia Iguarán hasta la maldición de Aureliano Babilonia transcurre todo un siglo. Cien años de soledad para una estirpe única, fantástica, capaz de fundar una ciudad tan especial como Macondo y de engendrar niños con cola de cerdo.</p>
