41b099992fa3777b5c20e221fd2c4665bf45e713
JossRV <jossrv74@gmail.com>
2022-11-22 22:44:30 -0600

    v1

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,7 +2,7 @@
 @extends('./layouts.plantilla')
 @section('contenido')
     <div class="container">
-        <div class="row mb-3">
+        <div class="row mb-3 align-items-center">
             <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs pe-3">
                 <div class="card mt-3">
                     <img src="{{ asset('img/portadas/diario.jpg') }}" class="card-img-top" alt="...">
@@ -33,8 +33,8 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row mb-3">
+        {{-- </div> --}}
+        {{-- <div class="row mb-3"> --}}
             <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
                 <div class="card mt-3">
                     <img src="{{ asset('img/portadas/emma.jpg') }}" class="card-img-top" alt="...">
@@ -65,6 +65,102 @@
                     </div>
                 </div>
             </div>
+        {{-- </div> --}}
+        {{-- <div class="row mb-3"> --}}
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+        {{-- </div> --}}
+        {{-- <div class="row mb-3"> --}}
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+        {{-- </div> --}}
+        {{-- <div class="row mb-3"> --}}
+            {{-- <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('img/portadas/emma.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('img/portadas/charlotte.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-6 col-xs ps-3">
+                <div class="card mt-3">
+                    <img src="{{ asset('img/portadas/fierecilla.jpeg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"></h5>
+                        <p class="card-text mb-5"></p>
+                        <a href="{{route('cienAnios')}}" class="btn btn-outline-success d-flex justify-content-center mb-2">Descubrelo</a>
+                    </div>
+                </div>
+            </div> --}}
         </div>
     </div>
 @endsection
